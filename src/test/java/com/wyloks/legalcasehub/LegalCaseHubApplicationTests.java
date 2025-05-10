84a49295e9d4a12220116e0daceb8fcc54001224
Will Okanumeh <willokans-mac-mini@192.168.1.189>
2025-05-10 20:36:50 +0100

    skeleton spring boot app-backend

---
+++ src/test/java/com/wyloks/legalcasehub/LegalCaseHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wyloks.legalcasehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegalCaseHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
